cf1ab30d55331dbb87361f7ffc570c9b5e625792
Irvi Aini <irvi.fa@gmail.com>
2022-07-20 17:11:15 +0200

    Initialize.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Distributions
+
+```
+pip -m unittest test
+```
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions-irvifa',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      install_requires=['matplotlib'],
+      zip_safe=False)
